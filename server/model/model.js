6aec10e89c3f0f5684e288bb1fc4af900ca5b1c9
pavan <ballapavan93@gmail.com>
2021-09-23 22:55:50 +0530

    Final commit

---
+++ server/model/model.js
@@ -0,0 +1,19 @@
+var mongoose=require('mongoose');
+
+var schema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    gender:String,
+    status:String
+})
+
+const Userdb=mongoose.model('Userdb',schema);
+
+module.exports=Userdb;
